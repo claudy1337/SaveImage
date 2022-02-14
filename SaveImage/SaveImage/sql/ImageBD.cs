56dc952d36e195d804801d15f6a203bf12960f08
diorblack <45602451+claudy1337@users.noreply.github.com>
2022-02-14 14:42:52 +0300

    Добавьте файлы проекта.

---
+++ SaveImage/SaveImage/sql/ImageBD.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using System.IO;
+
+namespace SaveImage.sql
+{
+    internal class ImageBD
+    {
+        SQLiteConnection db;
+        public ImageBD(string dbPath)
+        {
+            db = new SQLiteConnection(dbPath);
+            db.CreateTable<Image>();
+        }
+
+        public IEnumerable<Image> GetItems()
+        {
+            return db.Table<Image>().ToList();
+        }
+
+        public Image GetItem(int id)
+        {
+            return db.Get<Image>(id);
+        }
+
+        public int DeleteItem(int id)
+        {
+            return db.Delete<Image>(id);
+        }
+
+        public int SaveItem(Image item)
+        {
+            if (item.Id != 0)
+            {
+                db.Update(item);
+                return item.Id;
+            }
+            else
+            {
+                return db.Insert(item);
+            }
+        }
+    }
+}
